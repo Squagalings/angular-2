920a9a3b08f200261a277d544c97b9e6a1fd8c41
squagalings <justineyongjzxy@gmail.com>
2021-11-25 16:24:34 -0800

    lesson 24 up to data fuizhu

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {FilterPipe} from '../filter.pipe';
 import { LoggingService } from '../logging.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-directory',
   templateUrl: './directory.component.html',
-  styleUrls: ['./directory.component.css']
+  styleUrls: ['./directory.component.css'],
+  pipes: [FilterPipe]
 })
 export class DirectoryComponent implements OnInit {
+  ninjas = [];
+  bool = null;
 
-  ninjas = [
-    { name: 'Yoshi', belt: 'black' },
-    { name: 'Ryu', belt: 'red' },
-    { name: 'Crystal', belt: 'purple' }
-  ];
   constructor(private logger: LoggingService) { }
 
   logIt() {
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import './polyfills.ts';
-
+// import './polyfills.ts';
+import {bootstrap} from '@angular/platform-browser-dynamic';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
-import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
+import { AppComponent, environment } from 'app/app.routes';
+// import { AppModule } from './app/app.module';
 import { LoggingService } from './app/logging.service';
+import { HTTP_PROVIDERS} from '@angular/http';
 
 if (environment.production) {
   enableProdMode();
